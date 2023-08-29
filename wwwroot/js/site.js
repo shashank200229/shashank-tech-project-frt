fb95e745b2c0ebcc9e37437826517349c98450d6
shashank200229 <141292658+shashank200229@users.noreply.github.com>
2023-08-29 11:57:00 +0530

    Add files via upload

---
+++ wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+var link = document.querySelector("link[rel~='icon']");
+if (link) {
+    link = document.createElement('link');
+    link.rel = 'icon';
+    document.getElementsByTagName('head')[0].appendChild(link);
+    link.href = 'wwwroot/favicon.ico';
+}
